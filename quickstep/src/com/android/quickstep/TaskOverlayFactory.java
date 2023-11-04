67d2e8e771c44a86aafc0171acba160f0c923f39
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-11-04 17:17:04 +0530

    Launcher3: Go to home screen before launching lens activity
    Avoid recents getting stuck when Google Lens is "Unable to open image".
    
    Change-Id: I61ac95a8b4603b63b7d7c4edf73804c3e9738f91

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -212,6 +212,14 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
                 recentsView.dismissAllTasks(null, false);
         }
 
+        private void launchLens() {
+            final RecentsView recentsView = mThumbnailView.getTaskView().getRecentsView();
+            if (recentsView != null) {
+                recentsView.startHome();
+                mImageApi.startLensActivity();
+            }
+        }
+
         /**
          * Called when the overlay is no longer used.
          */
@@ -335,7 +343,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
 
             public void onLens() {
                 if (mIsAllowedByPolicy) {
-                    endLiveTileMode(() -> mImageApi.startLensActivity());
+                    endLiveTileMode(TaskOverlay.this::launchLens);
                 } else {
                     showBlockedByPolicyMessage();
                 }
